37a7178c9552f0ce5cd310f19283714440dc0a29
huynham <91304084+huynham@users.noreply.github.com>
2021-11-02 12:26:14 +0700

    upcode 2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TaskList from './component/TaskList';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <TaskList />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
